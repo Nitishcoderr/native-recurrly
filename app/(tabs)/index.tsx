1315af41f89aa80cdcfa3deec7a66a7b11d990b2
Your Name <nitishchaurasiyac555@gmail.com>
2026-04-19 02:39:58 +0530

    feat: project setup, nativewind, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
 import "@/global.css"
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
- 
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import {styled} from "nativewind";   
+const SafeAreaView = styled(RNSafeAreaView)
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -18,6 +21,6 @@ export default function App() {
         params: { id: "claude" }
       }} className="mt-4 rounded-full bg-accent px-4 py-2">Claude Subscriptions</Link>
       
-    </View>
+    </SafeAreaView>
   );
 }
